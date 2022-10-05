f0d5b18a8d61b9b27d2b5b61012571102c23bdd6
R8V <ignacio.slater@ug.uchile.cl>
2022-10-05 15:34:10 -0300

    TEST A Pokémon can be created with a name

---
+++ src/main/kotlin/cl/uchile/dcc/poke/Pokemon.kt
@@ -0,0 +1,12 @@
+/*
+ * "Bolsillos" (c) by R8V.
+ *  "Bolsillos" is licensed under a
+ *  Creative Commons Attribution 4.0 International License.
+ *  You should have received a copy of the license along with this
+ */
+
+package cl.uchile.dcc.poke
+
+class Pokemon(val name: String) {
+
+}
---
+++ src/test/kotlin/cl/uchile/dcc/poke/PokemonSpec.kt
@@ -0,0 +1,28 @@
+/*
+ * "Bolsillos" (c) by R8V.
+ *  "Bolsillos" is licensed under a
+ *  Creative Commons Attribution 4.0 International License.
+ *  You should have received a copy of the license along with this
+ */
+
+package cl.uchile.dcc.poke
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+/*
+ * "pokemon-kt" (c) by R8V.
+ * "pokemon-kt" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+ */
+class PokemonSpec : FunSpec({
+    val salandit = Pokemon("Salandit")
+    val scolipede = Pokemon("Scolipede")
+
+    test("A Pokémon can be created with a name") {
+        salandit.name shouldBe "Salandit"
+        scolipede.name shouldBe "Scolipede"
+    }
+})
