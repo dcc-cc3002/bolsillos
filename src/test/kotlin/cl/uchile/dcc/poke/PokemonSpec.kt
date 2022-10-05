5513d94b852e5cdabeaf42db3d1f62787fb0f6fb
R8V <ignacio.slater@ug.uchile.cl>
2022-10-05 16:38:34 -0300

    FEAT/TEST Pokémon stats

--- src/test/kotlin/cl/uchile/dcc/poke/PokemonSpec.kt
+++ src/test/kotlin/cl/uchile/dcc/poke/PokemonSpec.kt
@@ -9,25 +9,30 @@ package cl.uchile.dcc.poke
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 
-/*
- * "pokemon-kt" (c) by R8V.
- * "pokemon-kt" is licensed under a
- * Creative Commons Attribution 4.0 International License.
- * You should have received a copy of the license along with this
- * work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
- */
 class PokemonSpec : FunSpec({
     lateinit var salandit: Pokemon
     lateinit var scolipede: Pokemon
 
     beforeEach {
-        salandit = Pokemon("Salandit")
-        scolipede = Pokemon("Scolipede")
+        salandit = Pokemon("Salandit", 48, 44)
+        scolipede = Pokemon("Scolipede", 60, 100)
     }
 
-    test("A Pokémon can be created with a name") {
-        salandit.name shouldBe "Salandit"
-        scolipede.name shouldBe "Scolipede"
+    test("Two Pokémon with the same parameters should be equal") {
+        val salandit2 = Pokemon("Salandit", 48, 44)
+        salandit shouldNotBeSameInstanceAs salandit2
+        salandit shouldBe salandit2
+        val scolipede2 = Pokemon("Scolipede", 60, 100)
+        scolipede shouldNotBeSameInstanceAs scolipede2
+        scolipede shouldBe scolipede2
+
+    }
+
+    test("Two Pokémon with different parameters should not be equal") {
+        salandit shouldNotBeSameInstanceAs scolipede
+        salandit shouldNotBe scolipede
     }
 })
