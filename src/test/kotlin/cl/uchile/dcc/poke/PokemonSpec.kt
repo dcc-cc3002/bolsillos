1bdf86cd9bb8002cc3bda7971cd8915a37748602
R8V <ignacio.slater@ug.uchile.cl>
2022-10-06 22:03:17 -0300

    TEST Two Pokémon with the same parameters should have the same hash code

--- src/test/kotlin/cl/uchile/dcc/poke/PokemonSpec.kt
+++ src/test/kotlin/cl/uchile/dcc/poke/PokemonSpec.kt
@@ -8,8 +8,11 @@
 package cl.uchile.dcc.poke
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldHave
 import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.haveSameHashCodeAs
 import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 
 class PokemonSpec : FunSpec({
@@ -28,7 +31,13 @@ class PokemonSpec : FunSpec({
         val scolipede2 = Pokemon("Scolipede", 60, 100)
         scolipede shouldNotBeSameInstanceAs scolipede2
         scolipede shouldBe scolipede2
+    }
 
+    test("Two Pokémon with the same parameters should have the same hash code") {
+        val salandit2 = Pokemon("Salandit", 48, 44)
+        salandit should haveSameHashCodeAs(salandit2)
+        val scolipede2 = Pokemon("Scolipede", 60, 100)
+        scolipede should haveSameHashCodeAs(scolipede2)
     }
 
     test("Two Pokémon with different parameters should not be equal") {
