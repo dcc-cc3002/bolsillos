1b30360c5333fc95cc1fafeb965263c07619a36d
R8V <ignacio.slater@ug.uchile.cl>
2022-10-06 21:44:44 -0300

    DOC Pokémon class is documented

--- src/main/kotlin/cl/uchile/dcc/poke/Pokemon.kt
+++ src/main/kotlin/cl/uchile/dcc/poke/Pokemon.kt
@@ -9,6 +9,15 @@ package cl.uchile.dcc.poke
 
 import java.util.*
 
+/**
+ * A Pokémon is a creature that lives in the wild and can be captured by a trainer.
+ *
+ * @property name       The name of the Pokémon.
+ * @property maxHp      The maximum amount of HP the Pokémon can have.
+ * @property strength   The strength of the Pokémon.
+ *
+ * @constructor Creates a new Pokémon with the given name, maximum HP and strength.
+ */
 class Pokemon(val name: String, val maxHp: Int, val strength: Int) {
     override fun equals(other: Any?) = when {
         this === other -> true
